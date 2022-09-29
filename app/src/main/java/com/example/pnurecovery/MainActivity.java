09f9b9f019da578259c9982527099a82e7a771bc
흠냐룽 <limiabc@naver.com>
2022-09-29 13:47:56 +0900

    1.0.5

--- app/src/main/java/com/example/pnurecovery/MainActivity.java
+++ app/src/main/java/com/example/pnurecovery/MainActivity.java
@@ -2,17 +2,22 @@ package com.example.pnurecovery;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import java.lang.Math;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -23,11 +28,21 @@ import android.widget.TextView;
 import android.content.Context;
 import android.widget.Toast;
 
+import org.tensorflow.lite.Interpreter;
+
 import com.bumptech.glide.Glide;
 import com.example.pnurecovery.StickBarStrategyPattern.BrightnessButton;
 import com.example.pnurecovery.StickBarStrategyPattern.ContrastButton;
 import com.example.pnurecovery.StickBarStrategyPattern.SaturationButton;
+import com.soundcloud.android.crop.Crop;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -39,7 +54,10 @@ public class MainActivity extends AppCompatActivity {
     int edit_detail_on = 0;
     int save_count = 0;
     int stick_bar_on = 0;
+
     ImageView imageView;
+    ImageView resultView;
+
 
     public Uri imageSave() {
         ImageView img = (ImageView) findViewById(R.id.imageView);
@@ -106,6 +124,12 @@ public class MainActivity extends AppCompatActivity {
         cut_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent,
+                        1);
+
                 removeStickBar();
                 checkTurnOffEditDetail();
             }
@@ -254,6 +278,7 @@ public class MainActivity extends AppCompatActivity {
         Button save_button = (Button) findViewById(R.id.image_save_button);
 
         imageView = findViewById(R.id.imageView);
+        resultView = findViewById(R.id.imageview);
 
         edit_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -301,7 +326,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 turnOffAllAdditionalButton();
-                
+
                 // 갤러리 접근
                 Intent intent = new Intent();
                 intent.setType("image/*");
@@ -315,6 +340,62 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 turnOffAllAdditionalButton();
                 turnOnSRButton();
+
+                ImageView img;
+                Bitmap bmp;
+
+                img = (ImageView) activity.findViewById(R.id.imageView);
+
+                img.destroyDrawingCache();
+                img.setDrawingCacheEnabled(true);
+                bmp = img.getDrawingCache();
+
+                Bitmap bitmap = Bitmap.createScaledBitmap(bmp, 50, 50, true);
+                ByteBuffer input = ByteBuffer.allocateDirect(50 * 50 * 3 * 4).order(ByteOrder.nativeOrder());
+                for (int y = 0; y < 50; y++) {
+                    for (int x = 0; x < 50; x++) {
+                        int px = bitmap.getPixel(x, y);
+
+                        // Get channel values from the pixel value.
+                        int r = Color.red(px);
+                        int g = Color.green(px);
+                        int b = Color.blue(px);
+
+                        input.putFloat((float)r);
+                        input.putFloat((float)g);
+                        input.putFloat((float)b);
+                    }
+                }
+                System.out.println("*****************************************************");
+                System.out.println(input);
+                System.out.println("*****************************************************");
+
+                ByteBuffer modelOutput = ByteBuffer.allocateDirect(200 * 200 * 3 * 4).order(ByteOrder.nativeOrder());
+
+                Interpreter tflite = getTfliteInterpreter("ESRGAN.tflite");
+                tflite.run(input, modelOutput);
+
+                modelOutput.rewind();
+
+                int outputWidth = 200;
+                int outputHeight = 200;
+
+                Bitmap bitmap2 = Bitmap.createBitmap(outputWidth, outputHeight, Bitmap.Config.ARGB_8888);
+
+                int [] pixels = new int[outputWidth * outputHeight];
+                for (int i = 0; i < outputWidth * outputHeight; i++) {
+                    int a = 0xFF;
+
+                    float r = (modelOutput.getFloat());
+                    float g = (modelOutput.getFloat());
+                    float b = (modelOutput.getFloat());
+
+                    pixels[i] = a << 24 | ((int) r << 16) | ((int) g << 8) | (int) b;
+                }
+
+                bitmap2.setPixels(pixels, 0, outputWidth, 0, 0, outputWidth, outputHeight);
+
+                img.setImageBitmap(bitmap2);
             }
         });
 
@@ -326,14 +407,86 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    // 이미지 로드
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    // crop
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_select) {
+            resultView.setImageDrawable(null);
+            Crop.pickImage(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent result) {
+        super.onActivityResult(requestCode, resultCode, result);
         if (requestCode == 0) {
             if (resultCode == RESULT_OK) {
-                Glide.with(getApplicationContext()).load(data.getData()).override(1000, 1000).into(imageView);
+                Glide.with(getApplicationContext()).load(result.getData()).override(1000, 1000).into(imageView);
+                Toast.makeText(activity, "이미지로드", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else if (requestCode == 1) {
+
+            if (resultCode == RESULT_OK) {
+                //Glide.with(getApplicationContext()).load(result.getData()).override(500, 500).into(resultView);
+                Toast.makeText(activity, "Crop", Toast.LENGTH_SHORT).show();
+                beginCrop(result.getData());
+
+            } else if (requestCode == Crop.REQUEST_CROP) {
+                handleCrop(resultCode, result);
+
             }
         }
     }
 
+    private void beginCrop(Uri source) {
+        Uri destination = Uri.fromFile(new File(getCacheDir(), "cropped"));
+        Crop.of(source, destination).asSquare().start(this);
+    }
+
+    private void handleCrop(int resultCode, Intent result) {
+        if (resultCode == RESULT_OK) {
+            resultView.setImageURI(Crop.getOutput(result));
+        }
+        else if (resultCode == Crop.RESULT_ERROR) {
+            Toast.makeText(this, Crop.getError(result).getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+    // crop end
+
+    private Interpreter getTfliteInterpreter(String modelPath) {
+        try {
+            return new Interpreter(loadModelFile(MainActivity.this, modelPath));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private MappedByteBuffer loadModelFile(Activity activity, String modelPath) throws IOException {
+        AssetFileDescriptor fileDescriptor = activity.getAssets().openFd(modelPath);
+        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = inputStream.getChannel();
+        long startOffset = fileDescriptor.getStartOffset();
+        long declaredLength = fileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+    }
+
+
+    // 이미지 로드
+//    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode == 0) {
+//            if (resultCode == RESULT_OK) {
+//                Glide.with(getApplicationContext()).load(data.getData()).override(1000, 1000).into(imageView);
+//
+//            }
+//        }
+//    }
 }
